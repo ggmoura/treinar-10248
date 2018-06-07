4bd34b2f0deddcb332183090390b38c31485b719
Treinar <Treinar@SL05PROFESSOR>
2018-06-07 19:25:51 -0300

    renomeando projeto

--- primeiro-projeto/src/br/com/treinar/aula/TesteObjeto.java
+++ primeiro-projeto/src/br/com/treinar/aula/TesteObjeto.java
@@ -1,7 +1,18 @@
 package br.com.treinar.aula;
 
 public class TesteObjeto {
+	
 	public static void main(String[] args) {
-		
+		Conta c = new Conta();
+		c.cliente = new Cliente();
+		c.cliente.nome = "Maria Sophia";
+		c.saldo = 200000;
+		c.numero = 10481;
+		Conta c2 = c;
+		System.out.println(c2.numero);
+		c = new Conta();
+		System.out.println(c.numero);
+		c2 = new Conta();
+		System.out.println(c2.cliente.nome);
 	}
 }
