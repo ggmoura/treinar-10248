6d1474ae3ffeb425aacbfd74d33f949a4cc5bb6d
Gleidson <gleidson.gmoura@gmail.com>
2018-06-13 20:02:05 -0300

    conta

--- bb-gleidson/src/br/com/treinar/bb/model/banco/ContaPoupanca.java
+++ bb-gleidson/src/br/com/treinar/bb/model/banco/ContaPoupanca.java
@@ -1,8 +1,11 @@
 package br.com.treinar.bb.model.banco;
 
+import java.util.Calendar;
+
 public class ContaPoupanca extends Conta {
 
 	private float taxaRendimento;
+	private int diaUltimoDeposito;
 
 	public float getTaxaRendimento() {
 		return taxaRendimento;
@@ -10,6 +13,20 @@ public class ContaPoupanca extends Conta {
 
 	public void setTaxaRendimento(float taxaRendimento) {
 		this.taxaRendimento = taxaRendimento;
+	}
+
+	public int getDiaUltimoDeposito() {
+		return diaUltimoDeposito;
+	}
+
+	public void setDiaUltimoDeposito(int diaUltimoDeposito) {
+		this.diaUltimoDeposito = diaUltimoDeposito;
+	}
+
+	@Override
+	public void depositar(double valor) {
+		setSaldo(getSaldo() + valor);
+		diaUltimoDeposito = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
 	}
 
 }
