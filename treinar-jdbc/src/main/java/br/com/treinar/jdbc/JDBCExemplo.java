fe8d04480c8048b69312dc7398940f95c2cb80e7
ADM <ADM@SL01PROFESSOR>
2018-07-04 22:48:58 -0300

    jdbc

---
+++ treinar-jdbc/src/main/java/br/com/treinar/jdbc/JDBCExemplo.java
@@ -0,0 +1,13 @@
+package br.com.treinar.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCExemplo {
+	public static void main(String[] args) throws SQLException {
+		Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/contato", "root", "");
+		System.out.println("Conectado!");
+		conexao.close();
+	}
+}
